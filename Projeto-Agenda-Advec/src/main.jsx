b922e3b9450e8fa8f72a2ae76e1d1e5f47d20990
Henri <henri@gmail.com>
2024-11-08 17:09:05 -0300

    resolvendo erro

--- Projeto-Agenda-Advec/src/main.jsx
+++ Projeto-Agenda-Advec/src/main.jsx
@@ -4,5 +4,5 @@ import { StrictMode } from 'react'
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <App/>
-    </StrictMode>
+    </StrictMode>,
 )
--- Projeto-Agenda-Advec/src/pages/Escala.jsx
+++ Projeto-Agenda-Advec/src/pages/Escala.jsx
@@ -1,11 +1,57 @@
-function Escala () {
-    return(
-        <>
-        <div>
-            <h1>Escala</h1>
-        <p>Conteúdo Página</p>
-        </div>
-        </>
+import { React, useEffect, useState } from 'react'
+
+function Escala() {
+    const [nome, setNome] = useState('');
+    const [cultos, setCultos] = useState('')
+    const [nomeSalvo, setNomeSalvo] = useState('');
+    const [cultoSalvo, setCultoSalvo] = useState('')
+    const [estaCarregando, setEstaCarregando] = useState(true)
+
+
+    const toggleState = () => {
+        setEstaCarregando(prevState => !prevState);
+    }
+
+
+
+    const buscaNome = (event) => {
+        setNome(event.target.value);
+    };
+    const buscaCulto = (event) => {
+        setCultos(event.target.value);
+    }
+
+    const handleSalvarNome = (e) => {
+        e.preventDefault();
+        setNomeSalvo(nome);
+        setCultoSalvo(cultos)
+    };
+    const limpaCamposSalvos = (e) => {
+        e.preventDefault();
+        setNomeSalvo('');
+        setCultoSalvo('');
+    }
+
+
+    return (
+        <main>
+            {estaCarregando ? (<button onClick={toggleState}>Estou disponivel para escala</button>
+            ) : (
+                <form>
+                <h1>Escala da semana</h1>
+                <label htmlFor="">Seu nome:</label>
+                <input onChange={buscaNome} value={nome} type="text" />
+                <label htmlFor="">Culto:</label>
+                <input onChange={buscaCulto} value={cultos} type="text" />
+                <button onClick={handleSalvarNome}>Cadastrar</button>
+                <button onClick={limpaCamposSalvos}>Limpar</button>
+            </form>
+)
+            }
+            <aside>
+                {nomeSalvo && <h2>Nome salvo: {nomeSalvo} {cultoSalvo}</h2>}
+            </aside>
+        </main>
     )
 }
 export default Escala;
